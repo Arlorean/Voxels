1a1d036e1aa73b4caa9e56f10180d3a15a477b7e
Adam Davidson <adam@adam-davidson.com>
2017-08-06 11:24:27 +0100

    Add support for Ambient Occlusion. AO support when rendering to bitmap/png. SKSvgCanvas doesn't support DrawVertices so it wouldn't work for SVG ouput. Added a new test case and a couple of new text .vox files.

--- Voxels.SkiaSharp/Renderer.cs
+++ Voxels.SkiaSharp/Renderer.cs
@@ -9,7 +9,7 @@ namespace Voxels.SkiaSharp {
         static void RenderIntoBitmap(int size, VoxelData voxelData, SKBitmap bitmap) {
             using (var canvas = new SKCanvas(bitmap)) {
                 bitmap.Erase(SKColors.Transparent);
-                Render(size, voxelData, canvas);
+                Render(size, voxelData, canvas, ao: true);
             }
         }
 
@@ -39,33 +39,36 @@ namespace Voxels.SkiaSharp {
             using (var skStream = new SKManagedWStream(ms)) {
                 using (var writer = new SKXmlStreamWriter(skStream)) {
                     using (var canvas = SKSvgCanvas.Create(SKRect.Create(0, 0, size, size), writer)) {
-                        Render(size, voxelData, canvas);
+                        Render(size, voxelData, canvas, ao:false);
                     }
                 }
             }
             return ms.ToArray();
         }
 
-        static XYZ[] TopCorners = new[] {
-            new XYZ(-1, -1, +1),
-            new XYZ(-1, +1, +1),
-            new XYZ(+1, +1, +1),
-            new XYZ(+1, -1, +1),
+        static XYZ[] TopCross = new[] {
+            XYZ.OneY,
+            XYZ.OneX,
+            -XYZ.OneY,
+            -XYZ.OneX,
         };
-        static XYZ[] LeftCorners = new[] {
-            new XYZ(-1, -1, -1),
-            new XYZ(-1, +1, -1),
-            new XYZ(-1, +1, +1),
-            new XYZ(-1, -1, +1),
+        static XYZ[] LeftCross = new[] {
+            XYZ.OneY,
+            XYZ.OneZ,
+            -XYZ.OneY,
+            -XYZ.OneZ,
         };
-        static XYZ[] RightCorners = new[] {
-            new XYZ(-1, -1, -1),
-            new XYZ(-1, -1, +1),
-            new XYZ(+1, -1, +1),
-            new XYZ(+1, -1, -1),
+        static XYZ[] RightCross = new[] {
+            XYZ.OneZ,
+            XYZ.OneX,
+            -XYZ.OneZ,
+            -XYZ.OneX,
         };
+        static XYZ TopUp = XYZ.OneZ;
+        static XYZ RightUp = -XYZ.OneY;
+        static XYZ LeftUp = -XYZ.OneX;
 
-        static void Render(int size, VoxelData voxelData, SKCanvas canvas) {
+        static void Render(int size, VoxelData voxelData, SKCanvas canvas, bool ao) {
             var r = 1.61803398875f;
             var d = size / (r*voxelData.size.MaxDimension);
             var tran = SKMatrix44.CreateTranslate(size / 2, size/2,0);
@@ -85,13 +88,88 @@ namespace Voxels.SkiaSharp {
 
                         var color = voxelData.ColorOf(i);
                         if (color.A > 0) {
-                            RenderQuad(voxelData, i, canvas, matrix, color*0.8f, LeftCorners);
-                            RenderQuad(voxelData, i, canvas, matrix, color*0.9f, RightCorners);
-                            RenderQuad(voxelData, i, canvas, matrix, color, TopCorners);
+                            RenderQuad(voxelData, i, canvas, matrix, ao, color*0.8f, LeftCross, LeftUp);
+                            RenderQuad(voxelData, i, canvas, matrix, ao, color * 0.9f, RightCross, RightUp);
+                            RenderQuad(voxelData, i, canvas, matrix, ao, color, TopCross, TopUp);
                         }
                     }
                 }
             }
+        }
+
+        static void RenderQuad(VoxelData voxelData, XYZ p, SKCanvas canvas, SKMatrix44 matrix, bool ao, Color color, XYZ[] cross, XYZ up) {
+            // Only render quad if face it isn't hidden by voxel above it
+            if (voxelData[p + up].colorIndex == 0) {
+                // Map voxel coordinates to projected 3D space
+                
+                var vertices = Enumerable.Range(0, 4)
+                    .Select(i => cross[i]+cross[(i+1)%4]+up)
+                    .Select(c => c + XYZ.One)
+                    .Select(c => matrix.MapScalars(p.X + c.X * .5f, p.Z + c.Z * .5f, -p.Y - c.Y * .5f, 1f))
+                    .Select(v => new SKPoint(v[0], v[1]))
+                    .ToArray();
+
+                if (ao) {
+                    // Create face quad path
+                    var col = new SKColor(color.R, color.G, color.B, color.A);
+                    var a00 = AmbientOcclusion.CalculateAO(voxelData, p, cross[0], cross[1], up);
+                    var a01 = AmbientOcclusion.CalculateAO(voxelData, p, cross[1], cross[2], up);
+                    var a11 = AmbientOcclusion.CalculateAO(voxelData, p, cross[2], cross[3], up);
+                    var a10 = AmbientOcclusion.CalculateAO(voxelData, p, cross[3], cross[0], up);
+                    var colors = new SKColor[] {
+                        AOToColor(color, a00),
+                        AOToColor(color, a01),
+                        AOToColor(color, a11),
+                        AOToColor(color, a10),
+                    };
+
+                    ushort[] indices;
+                    if (a00 + a11 > a01 + a10) {
+                        indices = new ushort[] { 0,1,2, 2,3,0, };
+                    }
+                    else {
+                        indices = new ushort[] { 1,2,3, 3,0,1, };
+                    }
+
+                    // Calculate Ambient Occlusion
+                    using (var fill = new SKPaint {
+                        IsAntialias = false,
+                        Style = SKPaintStyle.Fill,
+                    }) {
+                        canvas.DrawVertices(SKVertexMode.Triangles, vertices, null, colors, indices, fill);
+                    }
+                }
+                else {
+                    // Create face quad path
+                    using (var face = new SKPath()) {
+                        face.AddPoly(vertices, close: true);
+
+                        // Calculate Ambient Occlusion
+                        using (var fill = new SKPaint {
+                            IsAntialias = false,
+                            Style = SKPaintStyle.Fill,
+                            Color = new SKColor(color.R, color.G, color.B, color.A),
+                        }) {
+                            canvas.DrawPath(face, fill);
+                        }
+                    }
+                }
+            }
+        }
+
+        static SKColor AOToColor(Color color, int ao) {
+            var c = new SKColor(color.R, color.G, color.B, color.A);
+            float h, s, v;
+            c.ToHsv(out h, out s, out v);
+
+            float r = 0;
+            switch (ao) {
+            case 0: r = 0.5f; break;
+            case 1: r = 0.75f; break;
+            case 2: r = 0.8f; break;
+            case 3: r = 1f; break;
+            }
+            return SKColor.FromHsv(h, s, v * r, c.Alpha);
         }
 
         static void RenderQuad(VoxelData voxelData, XYZ i, SKCanvas canvas, SKMatrix44 matrix, Color color, XYZ[] corners) {
--- Voxels.Test/TestAmbientOcclusion.cs
+++ Voxels.Test/TestAmbientOcclusion.cs
@@ -36,5 +36,18 @@ namespace Voxels.Test {
                 Assert.AreEqual(3, AmbientOcclusion.CalculateAO(voxelData, new XYZ(2, 2, 2), XYZ.OneY, XYZ.OneX, XYZ.OneZ));
             }
         }
+        [TestMethod]
+        public void TestCaseComplex() {
+            using (var stream = File.OpenRead("2x2x2.vox")) {
+                var voxelData = MagicaVoxel.Read(stream);
+                Assert.AreEqual(0, AmbientOcclusion.CalculateAO(voxelData, new XYZ(0, 0, 0), XYZ.OneY, XYZ.OneX, XYZ.OneZ));
+                Assert.AreEqual(0, AmbientOcclusion.CalculateAO(voxelData, new XYZ(0, 1, 1), XYZ.OneX, -XYZ.OneZ, -XYZ.OneY));
+                Assert.AreEqual(0, AmbientOcclusion.CalculateAO(voxelData, new XYZ(1, 0, 1), -XYZ.OneZ, XYZ.OneY, -XYZ.OneX));
+                Assert.AreEqual(2, AmbientOcclusion.CalculateAO(voxelData, new XYZ(0, 0, 0), -XYZ.OneX, XYZ.OneY, XYZ.OneZ));
+                Assert.AreEqual(2, AmbientOcclusion.CalculateAO(voxelData, new XYZ(0, 0, 0), XYZ.OneX, -XYZ.OneY, XYZ.OneZ));
+                Assert.AreEqual(3, AmbientOcclusion.CalculateAO(voxelData, new XYZ(0, 0, 0), -XYZ.OneX, -XYZ.OneY, XYZ.OneZ));
+            }
+        }
+
     }
 }

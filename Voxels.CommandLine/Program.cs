df6f605afbc9185610a24d502ec506154989a187
Adam Davidson <adam@adam-davidson.com>
2017-08-06 12:16:41 +0100

    Update website examples New Installer version 1.0.0.3 ready for release. Add example images for 8x8x8 and updated 3x3x3 with AO. Shrink my wizard example vox file so it fills the thumbnail and generate PNG and SVG images for it. Update mmmm and WindowsExplorer images for README.

--- Voxels.CommandLine/Program.cs
+++ Voxels.CommandLine/Program.cs
@@ -8,13 +8,12 @@ using Voxels.SkiaSharp;
 namespace Voxels.CommandLine {
     class Program {
         static void Main(string[] args) {
-            var filename = args.Length == 1 ? args[0] : "8x8x8.vox";
+            var filename = args.Length == 1 ? args[0] : "wizard.vox";
             using (var stream = File.OpenRead(filename)) {
                 var voxelData = MagicaVoxel.Read(stream);
 
-                var guid = Guid.NewGuid();
-                File.WriteAllBytes($"output{guid}.png", Renderer.RenderPng(512, voxelData));
-                File.WriteAllBytes($"output{guid}.svg", Renderer.RenderSvg(512, voxelData));
+                File.WriteAllBytes(Path.ChangeExtension(filename, ".png"), Renderer.RenderPng(512, voxelData));
+                File.WriteAllBytes(Path.ChangeExtension(filename, ".svg"), Renderer.RenderSvg(512, voxelData));
             }
         }
     }

ec1d69bfb849f9ae357e7b9f61bf234a7faa6104
Adam Davidson <adam@adam-davidson.com>
2017-08-17 10:28:53 +0100

    Update to v1.3

--- Voxels.Test/Properties/AssemblyInfo.cs
+++ Voxels.Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("44fd680f-9699-49d0-926c-e392b8cf521c")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
